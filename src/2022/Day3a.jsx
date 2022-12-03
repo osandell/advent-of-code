903179555d21e0c51739e63e73d463fc836796bc
Olof Sandell <83674246+osandell@users.noreply.github.com>
2022-12-03 07:21:07 +0100

    day 3 refined

--- src/2022/Day3a.jsx
+++ src/2022/Day3a.jsx
@@ -309,190 +309,28 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw`;
 
+const getCommonLetter = (s1, s2) => {
+  for (var i = 0; i < s1.length; i++) {
+    if (s2.includes(s1[i])) {
+      return s1[i];
+    }
+  }
+};
+
 export default () => {
-  let data = realData;
+  const result = realData
+    .split(/\n/)
+    .map((s) => {
+      const s1 = s.slice(0, s.length / 2);
+      const s2 = s.slice(s.length / 2, s.length);
+      const commonLetter = getCommonLetter(s1, s2);
 
-  data = data.split(/\n/).map((myString) => {
-    const partOne = myString.slice(0, myString.length / 2);
-    const partTwo = myString.slice(myString.length / 2, myString.length);
+      const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      const priority = alphabet.indexOf(commonLetter) + 1;
 
-    let commonLetter;
-
-    for (var i = 0; i < partOne.length; i++) {
-      commonLetter = partTwo.includes(partOne[i]) ? partOne[i] : null;
-      if (commonLetter) break;
-    }
-
-    let value;
-
-    switch (commonLetter) {
-      case "a":
-        value = 1;
-        break;
-      case "b":
-        value = 2;
-        break;
-      case "c":
-        value = 3;
-        break;
-      case "d":
-        value = 4;
-        break;
-      case "e":
-        value = 5;
-        break;
-      case "f":
-        value = 6;
-        break;
-      case "g":
-        value = 7;
-        break;
-      case "h":
-        value = 8;
-        break;
-      case "i":
-        value = 9;
-        break;
-      case "j":
-        value = 10;
-        break;
-      case "k":
-        value = 11;
-        break;
-      case "l":
-        value = 12;
-        break;
-      case "m":
-        value = 13;
-        break;
-      case "n":
-        value = 14;
-        break;
-      case "o":
-        value = 15;
-        break;
-      case "p":
-        value = 16;
-        break;
-      case "q":
-        value = 17;
-        break;
-      case "r":
-        value = 18;
-
-        break;
-      case "s":
-        value = 19;
-        break;
-      case "t":
-        value = 20;
-        break;
-      case "u":
-        value = 21;
-        break;
-      case "v":
-        value = 22;
-        break;
-      case "w":
-        value = 23;
-        break;
-      case "x":
-        value = 24;
-        break;
-      case "y":
-        value = 25;
-        break;
-      case "z":
-        value = 26;
-        break;
-      case "A":
-        value = 27;
-        break;
-      case "B":
-        value = 28;
-        break;
-      case "C":
-        value = 29;
-        break;
-      case "D":
-        value = 30;
-        break;
-      case "E":
-        value = 31;
-        break;
-      case "F":
-        value = 32;
-        break;
-      case "G":
-        value = 33;
-        break;
-      case "H":
-        value = 34;
-        break;
-      case "I":
-        value = 35;
-        break;
-      case "J":
-        value = 36;
-        break;
-      case "K":
-        value = 37;
-        break;
-      case "L":
-        value = 38;
-        break;
-      case "M":
-        value = 39;
-        break;
-      case "N":
-        value = 40;
-        break;
-      case "O":
-        value = 41;
-        break;
-      case "P":
-        value = 42;
-        break;
-      case "Q":
-        value = 43;
-        break;
-      case "R":
-        value = 44;
-        break;
-      case "S":
-        value = 45;
-        break;
-      case "T":
-        value = 46;
-        break;
-      case "U":
-        value = 47;
-        break;
-      case "V":
-        value = 48;
-        break;
-      case "W":
-        value = 49;
-        break;
-      case "X":
-        value = 50;
-        break;
-      case "Y":
-        value = 51;
-        break;
-      case "Z":
-        value = 52;
-        break;
-      default:
-        value = null;
-    }
-
-    return value;
-  });
-
-  let result = 0;
-
-  data.forEach((item) => (result += item));
+      return priority;
+    })
+    .reduce((acc, value) => acc + value);
 
   return <div>{result}</div>;
 };
