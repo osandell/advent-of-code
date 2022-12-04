51f702c545e42852952cb49524494ede40e6c539
Olof Sandell <83674246+osandell@users.noreply.github.com>
2022-12-04 07:19:20 +0100

    day 4

--- src/2022/Day4/Day4a.jsx
+++ src/2022/Day4/Day4a.jsx
@@ -3,7 +3,54 @@ import exampleData from "./exampleData";
 import realData from "./realData";
 
 export default () => {
-  const data = exampleData;
+  const data = realData
+    .split(/\n/)
+    .map((row) =>
+      row
+        .split(",")
+        .map((sectionSpan) =>
+          sectionSpan.split("-").map((number) => parseInt(number))
+        )
+    );
+
+  let nrOfPairsIncludeAll = 0;
+  data.forEach(([sectionsA, sectionsB]) => {
+    let secAList = [];
+    for (var i = sectionsA[0]; i <= sectionsA[1]; i++) {
+      secAList.push(i);
+    }
+    let secBList = [];
+
+    for (var i = sectionsB[0]; i <= sectionsB[1]; i++) {
+      secBList.push(i);
+    }
+
+    let aContainsAllB = true;
+    secAList.forEach((secA) => {
+      if (!secBList.includes(secA)) {
+        aContainsAllB = false;
+      }
+    });
+
+    let bContainsAllA = true;
+    secBList.forEach((secB) => {
+      if (!secAList.includes(secB)) {
+        bContainsAllA = false;
+      }
+    });
+
+    if (aContainsAllB || bContainsAllA) {
+      nrOfPairsIncludeAll++;
+    }
+  });
+
+  console.log(
+    "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c      nrOfPairsIncludeAll    \x1b[8m\x1b[40m\x1b[0m%c Day4a.jsx 49 \n",
+    "color: white; background: black; font-weight: bold",
+    "",
+    nrOfPairsIncludeAll
+  );
+
   const result = 0;
 
   return <div>{result}</div>;
--- src/2022/Day4/Day4b.jsx
+++ src/2022/Day4/Day4b.jsx
@@ -1,11 +1,56 @@
 import React from "react";
-
-const realData = ``;
-
-const exampleData = ``;
+import exampleData from "./exampleData";
+import realData from "./realData";
 
 export default () => {
-  const data = exampleData;
+  const data = realData
+    .split(/\n/)
+    .map((row) =>
+      row
+        .split(",")
+        .map((sectionSpan) =>
+          sectionSpan.split("-").map((number) => parseInt(number))
+        )
+    );
+
+  let nrOfPairsOverlaps = 0;
+  data.forEach(([sectionsA, sectionsB]) => {
+    let secAList = [];
+    for (var i = sectionsA[0]; i <= sectionsA[1]; i++) {
+      secAList.push(i);
+    }
+    let secBList = [];
+
+    for (var i = sectionsB[0]; i <= sectionsB[1]; i++) {
+      secBList.push(i);
+    }
+
+    let aOverlapsB = false;
+    secAList.forEach((secA) => {
+      if (secBList.includes(secA)) {
+        aOverlapsB = true;
+      }
+    });
+
+    let bOverlapsA = false;
+    secBList.forEach((secB) => {
+      if (secAList.includes(secB)) {
+        bOverlapsA = true;
+      }
+    });
+
+    if (aOverlapsB || bOverlapsA) {
+      nrOfPairsOverlaps++;
+    }
+  });
+
+  console.log(
+    "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c      nrOfPairsOverlaps    \x1b[8m\x1b[40m\x1b[0m%c Day4a.jsx 49 \n",
+    "color: white; background: black; font-weight: bold",
+    "",
+    nrOfPairsOverlaps
+  );
+
   const result = 0;
 
   return <div>{result}</div>;
