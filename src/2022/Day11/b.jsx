dfefce33ac4d4ffc7a17e3d72ce8985c61cecd90
Olof Sandell <83674246+osandell@users.noreply.github.com>
2022-12-11 22:39:40 +0100

    --wip-- [skip ci]

--- src/2022/Day11/b.jsx
+++ src/2022/Day11/b.jsx
@@ -5,16 +5,100 @@ import Render from "../../Render";
 
 export default () => {
   for (let k = 1; k < 2; k += 1) {
-    let row20correct = false;
+    // const data = eData
+    //   .split("\n\n")
+    //   .map((monkeyChunk) => monkeyChunk.split("\n"))
+    //   .map((line, index) => {
+    //     const funcs = [
+    //       (old) => old * 19,
+    //       (old) => old + 6,
+    //       (old) => old * old,
+    //       (old) => old + 3,
+    //     ];
+
+    //     return {
+    //       monkeyNr: parseInt(line[0].split(" ")[1]),
+    //       items: line[1]
+    //         .split(": ")[1]
+    //         .split(", ")
+    //         .map((nr) => parseInt(nr)),
+    //       operation: funcs[index],
+    //       test: parseInt(line[3].split("by ")[1]),
+    //       true: parseInt(line[4].split("monkey ")[1]),
+    //       false: parseInt(line[5].split("monkey ")[1]),
+    //       inspectedTimes: 0,
+    //     };
+    //   });
+
+    // for (let j = 0; j < 3; j++) {
+    //   for (let i = 0; i < data.length; i++) {
+    //     data[i].items.forEach((item) => {
+    //       let worryLevel = item;
+    //       i === 0 &&
+    //         j === 1 &&
+    //         console.log(
+    //           "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c      1          i 0 j 2    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 67 \n",
+    //           "color: white; background: black; font-weight: bold",
+    //           "",
+    //           true,
+    //           i,
+    //           data[i].test,
+    //           worryLevel
+    //         );
+
+    //       worryLevel = data[i].operation(worryLevel);
+
+    //       i === 0 &&
+    //         j === 1 &&
+    //         console.log(
+    //           "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c      2          i 0 j 2    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 67 \n",
+    //           "color: white; background: black; font-weight: bold",
+    //           "",
+    //           true,
+    //           i,
+    //           data[i].test,
+    //           worryLevel
+    //         );
+
+    //       data[i].inspectedTimes = data[i].inspectedTimes + 1;
+
+    //       if (worryLevel % data[i].test === 0) {
+    //         console.log(
+    //           "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c                true    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 44 \n",
+    //           "color: white; background: black; font-weight: bold",
+    //           "",
+    //           true
+    //         );
+    //         data[data[i].true].items.push(worryLevel);
+    //       } else {
+    //         data[data[i].false].items.push(worryLevel);
+    //       }
+    //     });
+    //     data[i].items = [];
+    //   }
+
+    //   // console.log(
+    //   //   // sumInspectedTimes,
+    //   //   data[0].inspectedTimes,
+    //   //   data[1].inspectedTimes,
+    //   //   data[2].inspectedTimes,
+    //   //   data[3].inspectedTimes
+    //   // );
+    // }
+
     const data = eData
       .split("\n\n")
       .map((monkeyChunk) => monkeyChunk.split("\n"))
       .map((line, index) => {
         const funcs = [
           (old) => old.mult.push(19),
-          (old) => old.add.push(6),
-          (old) => old.mult.push(old),
-          (old) => old.add.push(3),
+          (old) => {
+            old.add = old.add + 6;
+          },
+          (old) => old.mult.push(...old.mult),
+          (old) => {
+            old.add = old.add + 3;
+          },
         ];
 
         return {
@@ -37,149 +121,63 @@ export default () => {
     let trail;
     let yo = 0;
 
-    for (let j = 0; j < 20; j++) {
+    for (let j = 0; j < 3; j++) {
       for (let i = 0; i < data.length; i++) {
         data[i].items.forEach((item) => {
-          // let thisIsIt = false;
-          // if (item === trail) {
-          //   thisIsIt = true;
-          // }
-
-          // if (!trail && yo === 2) {
-          //   trail = item;
-          //   // console.log(
-          //   //   "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    trail    \x1b[8m\x1b[40m\x1b[0m\n",
-          //   //   "color: white; background: black; font-weight: bold",
-          //   //   trail
-          //   // );
-          //   thisIsIt = true;
-          // }
-          // yo++;
-
           let worryLevel = item;
 
-          j === test &&
+          i === 0 &&
+            j === 1 &&
             console.log(
-              "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c              worryLevel 1   \x1b[8m\x1b[40m\x1b[0m%c b.jsx 45 \n",
+              "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c     1           i 0 j 2    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 67 \n",
               "color: white; background: black; font-weight: bold",
               "",
-              worryLevel
+              "add",
+              worryLevel.add,
+              "mult",
+              worryLevel.mult,
+              "add + mult",
+              worryLevel.add + worryLevel.mult.reduce((a, b) => a * b)
             );
 
-          j === test &&
+          data[i].operation(worryLevel);
+
+          i === 0 &&
+            j === 1 &&
             console.log(
-              "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c              data[i].operation(worryLevel)    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 46 \n",
+              "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c     1           i 0 j 2    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 67 \n",
               "color: white; background: black; font-weight: bold",
               "",
-              data[i].operation
-            );
-
-          worryLevel = data[i].operation(worryLevel);
-
-          // if (thisIsIt) {
-          //   trail = worryLevel;
-          //   // console.log(
-          //   //   "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    trail    \x1b[8m\x1b[40m\x1b[0m\n",
-          //   //   "color: white; background: black; font-weight: bold",
-          //   //   trail,
-          //   //   data[i].operation
-          //   // );
-          // }
-
-          if (worryLevel > 1000000000000) {
-            // worryLevel = 3130708670094187;
-          }
-
-          j === test &&
-            console.log(
-              "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c              worryLevel 2   \x1b[8m\x1b[40m\x1b[0m%c b.jsx 45 \n",
-              "color: white; background: black; font-weight: bold",
-              "",
-              worryLevel
-            );
-
-          j === test &&
-            console.log(
-              "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c              data[i].test    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 66 \n",
-              "color: white; background: black; font-weight: bold",
-              "",
-              data[i].test
+              "add",
+              worryLevel.add,
+              "mult",
+              worryLevel.mult.reduce((a, b) => a * b),
+              "add + mult",
+              worryLevel.add + worryLevel.mult.reduce((a, b) => a * b)
             );
 
           data[i].inspectedTimes = data[i].inspectedTimes + 1;
 
           if (
-            worryLevel.mult.length > 0 &&
-            worryLevel.mult[0] % data[i].test === 0
+            (worryLevel.add + worryLevel.mult.reduce((a, b) => a * b)) %
+              data[i].test ===
+            0
           ) {
-            j === test &&
-              console.log(
-                "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c                true    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 76 \n",
-                "color: white; background: black; font-weight: bold",
-                "",
-                data[i].false
-              );
-
-            // if (thisIsIt) {
-            //   // console.log(
-            //   //   "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c                    true    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 118 \n",
-            //   //   "color: white; background: black; font-weight: bold",
-            //   //   "",
-            //   //   true,
-            //   //   data[i].test
-            //   // );
-            // }
             data[data[i].true].items.push(worryLevel);
           } else {
-            j === test &&
-              console.log(
-                "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c                false    \x1b[8m\x1b[40m\x1b[0m%c b.jsx 83 \n",
-                "color: white; background: black; font-weight: bold",
-                "",
-                data[i].false
-              );
             data[data[i].false].items.push(worryLevel);
           }
         });
         data[i].items = [];
       }
 
-      console.log(
-        // sumInspectedTimes,
-        data[0].inspectedTimes,
-        data[1].inspectedTimes,
-        data[2].inspectedTimes,
-        data[3].inspectedTimes
-      );
-
-      if (
-        data[0].inspectedTimes === 99 &&
-        data[1].inspectedTimes === 97 &&
-        data[2].inspectedTimes === 8 &&
-        data[3].inspectedTimes === 103
-      ) {
-        // alert("yoyo");
-        if (j === 19) {
-          row20correct = true;
-        }
-        console.log(
-          // sumInspectedTimes,
-          data[0].inspectedTimes,
-          data[1].inspectedTimes,
-          data[2].inspectedTimes,
-          data[3].inspectedTimes
-        );
-      }
-
-      if (j === 999 && row20correct) {
-        console.log(
-          // sumInspectedTimes,
-          data[0].inspectedTimes,
-          data[1].inspectedTimes,
-          data[2].inspectedTimes,
-          data[3].inspectedTimes
-        );
-      }
+      // console.log(
+      //   // sumInspectedTimes,
+      //   data[0].inspectedTimes,
+      //   data[1].inspectedTimes,
+      //   data[2].inspectedTimes,
+      //   data[3].inspectedTimes
+      // );
     }
   }
 
