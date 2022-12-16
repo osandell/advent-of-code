e5c937889a8f435b7df7eae28f594d384c3c192d
Olof Sandell <83674246+osandell@users.noreply.github.com>
2022-12-16 08:00:05 +0100

    wip day 16a

--- src/2022/Day16/a.jsx
+++ src/2022/Day16/a.jsx
@@ -9,147 +9,113 @@ const ROPE_LENGTH = 10;
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-const data = rData.split(/\n/).map((row) => row.split(""));
+const valves = eData.split(/\n/).map((row) => {
+  return {
+    valve: row.split("Valve ")[1].split(" ")[0],
+    rate: parseInt(row.split("Valve ")[1].split("rate=")[1].split(";")[0]),
+    tunnels: row.split("Valve ")[1].split("valves ")[1]?.split(", ") || [
+      row.split("Valve ")[1].split("valve ")[1],
+    ],
+  };
+});
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-const isAdjecent = (part1, part2) =>
-  (part1[0] === part2[0] ||
-    part1[0] === part2[0] - 1 ||
-    part1[0] === part2[0] + 1) &&
-  (part1[1] === part2[1] ||
-    part1[1] === part2[1] - 1 ||
-    part1[1] === part2[1] + 1);
+console.log(
+  "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    data    \x1b[8m\x1b[40m\x1b[0m%c a.jsx 17 \n",
+  "color: white; background: black; font-weight: bold",
+  "",
+  valves
+);
 
 export default () => {
-  let currMove = 0;
-
-  // let totalNrOfMoves = 0;
-  // for (let i = 0; i < data.length; i++) {
-  //   const move = data[i];
-  //   const nrOfMovesInCurrentDirection = move[1];
-  //   for (let i = 0; i < nrOfMovesInCurrentDirection; i++) {
-  //     totalNrOfMoves++;
-  //   }
-  // }
-  const totalNrOfMoves = 99999999999999999999999999;
-
-  const [moveNr, setMoveNr] = useState(0);
-  const moveNrRef = React.useRef(moveNr);
-  moveNrRef.current = moveNr;
-
-  const startPlaying = () => {
-    if (moveNr < totalNrOfMoves) {
-      const timer = setInterval(() => {
-        moveNrRef.current < totalNrOfMoves
-          ? setMoveNr(moveNrRef.current + 1)
-          : clearInterval(timer);
-      }, 30);
-    }
-  };
-
-  // *********************************************************************************
-
   let result = 0;
 
-  for (let i = 0; i < moveNr; i++) {
-    data.forEach((row, rowIndex) => {
-      row.forEach((tile, tileIndex) => {});
+  let currentValveId = "AA";
+  let hasOpenedCurrent = false;
+  let totalPressure = 0;
+  for (let i = 0; i < 30; i++) {
+    let valve = valves.find((v) => v.valve === currentValveId);
+    let highestNextValveId = 0;
+    let highestTotalNextValveRate = 0;
+    let highestNextValveRate = 0;
+    let highestNextNextValveRate = 0;
+    valve.tunnels.forEach((nextValveId) => {
+      let nextValve = valves.find((v) => v.valve === nextValveId);
+      nextValve.tunnels.forEach((nextNextValveId) => {
+        let nextNextValve = valves.find((v) => v.valve === nextNextValveId);
+        if (nextNextValve.rate > highestNextNextValveRate) {
+          highestNextNextValveRate = nextNextValve.rate;
+        }
+      });
+      if (nextValve.rate > highestNextValveRate) {
+        highestNextValveRate = nextValve.rate;
+        if (
+          highestNextNextValveRate + highestNextValveRate >
+          highestTotalNextValveRate
+        ) {
+          highestNextValveId = nextValve.valve;
+          highestTotalNextValveRate =
+            highestNextNextValveRate + highestNextValveRate;
+        }
+      }
     });
+
+    // debugger;
+
+    if (
+      (valve.rate < highestNextValveRate &&
+        valve.rate < highestNextNextValveRate) ||
+      hasOpenedCurrent
+    ) {
+      currentValveId = highestNextValveId;
+      hasOpenedCurrent = false;
+    } else {
+      hasOpenedCurrent = true;
+      totalPressure += valve.rate;
+    }
   }
 
-  let dataToRender = [];
-  data.forEach((row) => {
-    let newRow = [];
-    row.forEach((tile) => {
-      newRow.push(tile);
-    });
-
-    dataToRender.push(newRow);
-  });
-
-  // *********************************************************************************
-
-  return (
-    <div>
-      <Render
-        dataToRender={dataToRender}
-        emptyTileIndicator={""}
-        shouldRenderBinarily={false}
-        shouldInvertX={false}
-        shouldInvertY={false}
-        sizeX={"20px"}
-        sizeY={"15px"}
-        isCenterOrigin={false}
-      />
-      <div style={{ marginTop: "24px" }}>
-        <button
-          onClick={() => moveNr > 0 && setMoveNr(0)}
-          style={{
-            marginRight: "8px",
-            color: moveNr > 0 ? "black" : "lightGray",
-          }}
-        >
-          Beginning
-        </button>
-        <button
-          onClick={() => moveNr > 9 && setMoveNr(moveNr - 10)}
-          style={{
-            marginRight: "8px",
-            color: moveNr > 9 ? "black" : "lightGray",
-          }}
-        >
-          Prev 10
-        </button>
-        <button
-          onClick={() => moveNr > 0 && setMoveNr(moveNr - 1)}
-          style={{
-            marginRight: "8px",
-            color: moveNr > 0 ? "black" : "lightGray",
-          }}
-        >
-          Prev
-        </button>
-        <button
-          onClick={() => startPlaying()}
-          style={{
-            marginRight: "8px",
-            color: moveNr < totalNrOfMoves ? "black" : "lightGray",
-          }}
-        >
-          Play
-        </button>
-        <button
-          onClick={() => moveNr < totalNrOfMoves && setMoveNr(moveNr + 1)}
-          style={{
-            marginRight: "8px",
-            color: moveNr < totalNrOfMoves ? "black" : "lightGray",
-          }}
-        >
-          Next
-        </button>
-        <button
-          onClick={() => moveNr < totalNrOfMoves - 9 && setMoveNr(moveNr + 10)}
-          style={{
-            marginRight: "8px",
-            color: moveNr < totalNrOfMoves - 9 ? "black" : "lightGray",
-          }}
-        >
-          Next 10
-        </button>
-        <button
-          onClick={() => moveNr < totalNrOfMoves && setMoveNr(totalNrOfMoves)}
-          style={{
-            marginRight: "8px",
-            color: moveNr < totalNrOfMoves ? "black" : "lightGray",
-          }}
-        >
-          End
-        </button>
-      </div>
-      <div style={{ marginTop: "24px" }}>Move nr: {moveNr}</div>
-      <div style={{ marginTop: "24px" }}>Result: {result}</div>
-    </div>
-  );
+  return <div>{totalPressure}</div>;
 };
+
+// const getTotalPressureFromValve = (valveName, minutesLeft) => {
+//   let maximumPressureSkippingCurrent = 0;
+//   let maximumPressureOpeningCurrent = 0;
+//   const valve = valves.find((v) => v.valve === valveName);
+//   valve.tunnels.forEach((tunnel) => {
+//     const nextValve = valves.find((v) => v.valve === tunnel);
+//     if (minutesLeft > 1) {
+//       let nextValveTotalPressure = getTotalPressureFromValve(
+//         nextValve.valve,
+//         minutesLeft - 1
+//       );
+//       if (nextValveTotalPressure > maximumPressureSkippingCurrent) {
+//         maximumPressureSkippingCurrent = nextValveTotalPressure;
+//       }
+//     } else if (minutesLeft === 1) {
+//       if (nextValve.rate > maximumPressureSkippingCurrent) {
+//         maximumPressureSkippingCurrent = nextValve.rate;
+//       }
+//     }
+
+//     if (minutesLeft > 2) {
+//       let nextValveTotalPressure = getTotalPressureFromValve(
+//         nextValve.valve,
+//         minutesLeft - 2
+//       );
+//       if (nextValveTotalPressure > maximumPressureOpeningCurrent) {
+//         maximumPressureOpeningCurrent = nextValveTotalPressure;
+//       }
+//     }
+//   });
+
+//   if (maximumPressureOpeningCurrent > maximumPressureSkippingCurrent) {
+//     return maximumPressureOpeningCurrent + valve.rate;
+//   } else {
+//     return maximumPressureSkippingCurrent;
+//   }
+// };
+
+// result = getTotalPressureFromValve("AA", 20);
