46c630b0906a9bf621d58531d9ccceebc46c22e3
Olof Sandell <83674246+osandell@users.noreply.github.com>
2022-12-18 07:30:41 +0100

    --wip-- [skip ci]

--- src/2022/Day18/a.jsx
+++ src/2022/Day18/a.jsx
@@ -9,10 +9,634 @@ const ROPE_LENGTH = 10;
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-const data = rData.split(/\n/).map((row) => row.split(""));
+const data = rData
+  .split(/\n/)
+  .map((row) => row.split(",").map((nr) => parseInt(nr)));
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+let sides = {};
+data.forEach((cube, rowIndex) => {
+  let front =
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ];
+
+  if (front !== undefined) {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ] = front + 1;
+  } else {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ] = 0;
+  }
+
+  let left =
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ];
+
+  if (left !== undefined) {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ] = left + 1;
+  } else {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ] = 0;
+  }
+
+  let right =
+    sides[
+      cube[0] +
+        1 +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ];
+
+  if (right !== undefined) {
+    sides[
+      cube[0] +
+        1 +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ] = right + 1;
+  } else {
+    sides[
+      cube[0] +
+        1 +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2]
+    ] = 0;
+  }
+
+  let back =
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1)
+    ];
+
+  if (back !== undefined) {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1)
+    ] = back + 1;
+  } else {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1)
+    ] = 0;
+  }
+
+  let top =
+    sides[
+      cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1)
+    ];
+
+  if (top !== undefined) {
+    sides[
+      cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1)
+    ] = top + 1;
+  } else {
+    sides[
+      cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        (cube[1] + 1) +
+        ":" +
+        (cube[2] + 1)
+    ] = 0;
+  }
+
+  let bottom =
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1)
+    ];
+
+  if (bottom !== undefined) {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1)
+    ] = bottom + 1;
+  } else {
+    sides[
+      cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        cube[2] +
+        "," +
+        (cube[0] + 1) +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1) +
+        "," +
+        cube[0] +
+        ":" +
+        cube[1] +
+        ":" +
+        (cube[2] + 1)
+    ] = 0;
+  }
+});
+
+const checkIsFrontOrBack = (coords) => {
+  let lastZ;
+  coords.forEach((coord) => {
+    let z = coord.split(":")[2];
+
+    if (lastZ && lastZ !== z) {
+      return false;
+    }
+
+    lastZ = z;
+  });
+  return true;
+};
+const checkIsLeftOrRight = (coords) => {
+  let lastX;
+  let isLorR = true;
+  coords.forEach((coord) => {
+    let x = coord.split(":")[0];
+
+    if (lastX && lastX !== x) {
+      isLorR = false;
+    }
+
+    lastX = x;
+  });
+  return isLorR;
+};
+const checkIsTopOrBottom = (coords) => {
+  let lastY;
+  coords.forEach((coord) => {
+    let y = coord.split(":")[0];
+
+    if (lastY && lastY !== y) {
+      return false;
+    }
+
+    lastY = y;
+  });
+  return true;
+};
+
+let total = 0;
+Object.keys(sides).forEach((value, index) => {
+  if (sides[value] === 0) {
+    total++;
+  }
+
+  let coords = value.split(",");
+  let isFrontOrBack = checkIsFrontOrBack(coords);
+
+  if (isFrontOrBack && index === 0) {
+    let coords = value
+      .split(",")
+      .map((nr) => nr.split(":").map((nr) => parseInt(nr)));
+
+    console.log(
+      "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c        coords    \x1b[8m\x1b[40m\x1b[0m%c a.jsx 566 \n",
+      "color: white; background: black; font-weight: bold",
+      "",
+      coords
+    );
+
+    let opposite1 =
+      coords[0][0] +
+      ":" +
+      coords[0][1] +
+      ":" +
+      (coords[0][2] + 1) +
+      "," +
+      coords[1][0] +
+      ":" +
+      coords[1][1] +
+      ":" +
+      (coords[1][2] + 1) +
+      "," +
+      coords[2][0] +
+      ":" +
+      coords[2][1] +
+      ":" +
+      (coords[2][2] + 1) +
+      "," +
+      coords[3][0] +
+      ":" +
+      coords[3][1] +
+      ":" +
+      (coords[3][2] + 1);
+
+    let opposite2 =
+      coords[0][0] +
+      ":" +
+      coords[0][1] +
+      ":" +
+      (coords[0][2] - 1) +
+      "," +
+      coords[1][0] +
+      ":" +
+      coords[1][1] +
+      ":" +
+      (coords[1][2] - 1) +
+      "," +
+      coords[2][0] +
+      ":" +
+      coords[2][1] +
+      ":" +
+      (coords[2][2] - 1) +
+      "," +
+      coords[3][0] +
+      ":" +
+      coords[3][1] +
+      ":" +
+      (coords[3][2] - 1);
+
+    console.log(
+      "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c          opposite1    \x1b[8m\x1b[40m\x1b[0m%c a.jsx 617 \n",
+      "color: white; background: black; font-weight: bold",
+      "",
+      opposite1
+    );
+
+    //opposite
+    if (sides[opposite1] || sides[opposite2]) {
+      console.log(
+        "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    hasopp    \x1b[8m\x1b[40m\x1b[0m%c a.jsx 625 \n",
+        "color: white; background: black; font-weight: bold",
+        ""
+      );
+    }
+  }
+});
 
 const isAdjecent = (part1, part2) =>
   (part1[0] === part2[0] ||
