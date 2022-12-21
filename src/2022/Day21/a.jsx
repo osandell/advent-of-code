33776518f8aac7a429261b2ffa83e47c664f3dd4
Olof Sandell <83674246+osandell@users.noreply.github.com>
2022-12-21 07:25:23 +0100

    21a first working

--- src/2022/Day21/a.jsx
+++ src/2022/Day21/a.jsx
@@ -9,10 +9,28 @@ const ROPE_LENGTH = 10;
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-const data = rData.split(/\n/);
+
+let eeData = `root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32`;
+
+const data = eeData.split(/\n/);
 
 let monkeys = {};
-for (let i = 0; i < 5000; i++) {
+for (let i = 0; i < 500; i++) {
+  // if (i % 10 === 0) console.log(i);
   monkeys = {};
   data.forEach((row, rowIndex) => {
     let number = row.split(":")[0] === "humn" ? i : parseInt(row.split(" ")[1]);
@@ -86,6 +104,12 @@ for (let i = 0; i < 5000; i++) {
     );
   }
 }
+
+console.log(
+  "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    done    \x1b[8m\x1b[40m\x1b[0m%c a.jsx 90 \n",
+  "color: white; background: black; font-weight: bold",
+  ""
+);
 
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
