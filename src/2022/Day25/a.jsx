85dc7c7bf4682a08e48f029f06a8949ce96b0937
Olof Sandell <83674246+osandell@users.noreply.github.com>
2022-12-26 16:57:06 +0100

    day25 first working

--- src/2022/Day25/a.jsx
+++ src/2022/Day25/a.jsx
@@ -60,85 +60,185 @@ const getSnafuArrFromDecimal = (decimal) => {
   return [snafu].concat([rest.toString()]);
 };
 
+const shiftUp = (totalSnafu, index) => {
+  let foundNon2 = false;
+  for (let i = index - 1; i >= 0; i--) {
+    let char = totalSnafu[i];
+    if (char === "2") {
+      totalSnafu[i] = "=";
+    } else {
+      if (!foundNon2) {
+        switch (totalSnafu[i]) {
+          case "=":
+            totalSnafu[i] = "-";
+            break;
+          case "-":
+            totalSnafu[i] = "0";
+            break;
+          case "0":
+            totalSnafu[i] = "1";
+            break;
+          case "1":
+            totalSnafu[i] = "2";
+            break;
+        }
+        foundNon2 = true;
+        break;
+      }
+    }
+  }
+  if (!foundNon2) {
+    totalSnafu.unshift("1");
+  }
+};
+
+const shiftDown = (totalSnafu, index) => {
+  let foundNonDoubleMinus = false;
+  for (let i = index - 1; i >= 0; i--) {
+    let char = totalSnafu[i];
+    if (char === "=") {
+      totalSnafu[i] = "2";
+    } else {
+      if (!foundNonDoubleMinus) {
+        switch (totalSnafu[i]) {
+          case "-":
+            totalSnafu[i] = "=";
+            break;
+          case "0":
+            totalSnafu[i] = "-";
+            break;
+          case "1":
+            totalSnafu[i] = "0";
+            break;
+          case "2":
+            totalSnafu[i] = "1";
+            break;
+        }
+        foundNonDoubleMinus = true;
+        break;
+      }
+    }
+  }
+};
+
 const getSnafuFromSnfuArr = (snafuArr) => {
+  let test = [...snafuArr];
   let totalSnafu = snafuArr.splice(0, 1)[0].split("");
-  // debugger;
   snafuArr.forEach((snafu) => {
+    let test2 = [...test];
     let index = totalSnafu.length - snafu.length;
     let foundNon2 = false;
     switch (totalSnafu[index]) {
       case "=":
-        totalSnafu[index] = snafu.length === 1 ? snafu : "0";
+        if (snafu.length === 1) {
+          switch (snafu) {
+            case "=":
+              totalSnafu[index] = "1";
+              shiftDown(totalSnafu, index);
+              break;
+            case "-":
+              totalSnafu[index] = "2";
+              shiftDown(totalSnafu, index);
+              break;
+            case "1":
+              totalSnafu[index] = "-";
+              break;
+            case "2":
+              totalSnafu[index] = "0";
+              break;
+          }
+        } else {
+          totalSnafu[index] = "0";
+        }
+
         break;
       case "-":
-        totalSnafu[index] = snafu.length === 1 ? snafu : "1";
+        if (snafu.length === 1) {
+          switch (snafu) {
+            case "=":
+              totalSnafu[index] = "2";
+              shiftDown(totalSnafu, index);
+              break;
+            case "-":
+              totalSnafu[index] = "=";
+              break;
+            case "1":
+              totalSnafu[index] = "0";
+              break;
+            case "2":
+              totalSnafu[index] = "1";
+              break;
+          }
+        } else {
+          totalSnafu[index] = "1";
+        }
         break;
       case "0":
-        totalSnafu[index] = snafu.length === 1 ? snafu : "2";
+        if (snafu.length === 1) {
+          switch (snafu) {
+            case "=":
+              totalSnafu[index] = "=";
+              break;
+            case "-":
+              totalSnafu[index] = "-";
+              break;
+            case "1":
+              totalSnafu[index] = "1";
+              break;
+            case "2":
+              totalSnafu[index] = "2";
+              break;
+          }
+        } else {
+          totalSnafu[index] = "2";
+        }
         break;
       case "1":
-        totalSnafu[index] = snafu.length === 1 ? snafu : "=";
-        for (let i = index - 1; i >= 0; i--) {
-          let char = totalSnafu[i];
-          if (char === "2") {
-            totalSnafu[i] = "=";
-          } else {
-            if (!foundNon2) {
-              switch (totalSnafu[i]) {
-                case "=":
-                  totalSnafu[i] = "-";
-                  break;
-                case "-":
-                  totalSnafu[i] = "0";
-                  break;
-                case "0":
-                  totalSnafu[i] = "1";
-                  break;
-                case "1":
-                  totalSnafu[i] = "2";
-                  break;
-              }
-              foundNon2 = true;
+        if (snafu.length === 1) {
+          switch (snafu) {
+            case "=":
+              totalSnafu[index] = "-";
               break;
-            }
+            case "-":
+              totalSnafu[index] = "0";
+              break;
+            case "1":
+              totalSnafu[index] = "2";
+              break;
+            case "2":
+              totalSnafu[index] = "=";
+              shiftUp(totalSnafu, index);
+              break;
           }
+        } else {
+          totalSnafu[index] = "=";
+          shiftUp(totalSnafu, index);
         }
 
-        if (!foundNon2) {
-          totalSnafu.unshift("1");
-        }
         break;
       case "2":
-        totalSnafu[index] = "-";
-        for (let i = index - 1; i >= 0; i--) {
-          let char = totalSnafu[i];
-          if (char === "2") {
-            totalSnafu[i] = "=";
-          } else {
-            if (!foundNon2) {
-              switch (totalSnafu[i]) {
-                case "=":
-                  totalSnafu[i] = "-";
-                  break;
-                case "-":
-                  totalSnafu[i] = "0";
-                  break;
-                case "0":
-                  totalSnafu[i] = "1";
-                  break;
-                case "1":
-                  totalSnafu[i] = "2";
-                  break;
-              }
-              foundNon2 = true;
+        if (snafu.length === 1) {
+          switch (snafu) {
+            case "=":
+              totalSnafu[index] = "0";
               break;
-            }
+            case "-":
+              totalSnafu[index] = "1";
+              break;
+            case "1":
+              totalSnafu[index] = "=";
+              shiftUp(totalSnafu, index);
+              break;
+            case "2":
+              totalSnafu[index] = "-";
+              shiftUp(totalSnafu, index);
+              break;
           }
+        } else {
+          totalSnafu[index] = "-";
+          shiftUp(totalSnafu, index);
         }
 
-        if (!foundNon2) {
-          totalSnafu.unshift("1");
-        }
         break;
     }
   });
@@ -154,14 +254,14 @@ export default () => {
   let test = getDecimalFromSnafu(["1", "=", "1", "2", "2", "2"]);
   test = getDecimalFromSnafu(["1", "=", "1", "1", "-", "2"]);
   test = getDecimalFromSnafu(["2", "2", "2", "2", "2"]);
-  test = getDecimalFromSnafu(["2", "0", "2", "0"]);
+  test = getDecimalFromSnafu(["2", "=", "=", "0"]);
+  console.log(
+    "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    test    \x1b[8m\x1b[40m\x1b[0m\n",
+    "color: white; background: black; font-weight: bold",
+    test
+  );
 
   let snafuArr = getSnafuArrFromDecimal(totalDecimal);
-  console.log(
-    "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    snafuArr    \x1b[8m\x1b[40m\x1b[0m\n",
-    "color: white; background: black; font-weight: bold",
-    snafuArr
-  );
   let snafu = getSnafuFromSnfuArr(snafuArr);
   console.log(
     "\x1b[8m\x1b[40m\x1b[0m\x1b[7m%c    snafu    \x1b[8m\x1b[40m\x1b[0m\n",
